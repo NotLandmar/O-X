8b12378c237789ebb3302e9e1466098ccff0ff85
NotLandmar <67385683+NotLandmar@users.noreply.github.com>
2020-06-27 22:48:23 +0300

    O X

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.jws.soap.SOAPBinding;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("O O X");
+        System.out.println("O X O");
+        System.out.println("X O O");
+    }
+}
